9cbe763021c0b03e1a109be0b2afe19be9fbb7f4
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-10-13 15:01:49 +0700

    feat: revamped header and showed a mock wallet address

--- components/header.tsx
+++ components/header.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="container mx-auto py-5 sm:py-10">
-      <header className="flex items-center md:justify-between gap-x-5">
-        <Link href="/" className="flex items-center space-x-2 z-[100]">
-          {/* <img src="" /> */}
-          <span className="text-base font-bold md:inline mr-10 opacity-90">
-            AnyApe
-          </span>
+    <div className="flex w-full items-center justify-between my-4">
+      <div className="flex items-center">
+        <Link href="/">
+          <span className="text-xl">AnyApe</span>{" "}
+          {/* TODO: CHANGE INTO LOGO LATER */}
         </Link>
-      </header>
+        <span className="text-base ml-4">Marketplace</span>
+      </div>
+      <button>0xeD7B...6Afd</button>
     </div>
   );
 }
