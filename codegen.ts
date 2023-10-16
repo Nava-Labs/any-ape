1dfd7342930298f1770d3f957f435b14b4a204a7
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-10-16 21:49:05 +0700

    feat: created codegen config to generate types for graphql

---
+++ codegen.ts
@@ -0,0 +1,15 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'https://api.thegraph.com/subgraphs/name/jonassunandar/any-ape-subgraph',
+  documents: ['./lib/gql/queries/nft.ts'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './lib/gql/types/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+};
+
+export default config;
