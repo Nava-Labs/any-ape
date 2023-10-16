5ffac0f2303f90d6f0f1cd2d38569641f3613322
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-10-16 21:49:49 +0700

    feat: generated types for graphql

---
+++ lib/gql/types/graphql.ts
@@ -0,0 +1,418 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  BigDecimal: { input: any; output: any; }
+  BigInt: { input: any; output: any; }
+  Bytes: { input: any; output: any; }
+  /**
+   * 8 bytes signed integer
+   *
+   */
+  Int8: { input: any; output: any; }
+};
+
+export type Activity = {
+  __typename?: 'Activity';
+  from: Scalars['Bytes']['output'];
+  id: Scalars['ID']['output'];
+  nft: Nft;
+  price?: Maybe<Scalars['BigInt']['output']>;
+  timestamp: Scalars['BigInt']['output'];
+  to: Scalars['Bytes']['output'];
+  type: Scalars['String']['output'];
+};
+
+export type Activity_Filter = {
+  /** Filter for the block changed event. */
+  _change_block?: InputMaybe<BlockChangedFilter>;
+  and?: InputMaybe<Array<InputMaybe<Activity_Filter>>>;
+  from?: InputMaybe<Scalars['Bytes']['input']>;
+  from_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  from_gt?: InputMaybe<Scalars['Bytes']['input']>;
+  from_gte?: InputMaybe<Scalars['Bytes']['input']>;
+  from_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  from_lt?: InputMaybe<Scalars['Bytes']['input']>;
+  from_lte?: InputMaybe<Scalars['Bytes']['input']>;
+  from_not?: InputMaybe<Scalars['Bytes']['input']>;
+  from_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  from_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  id?: InputMaybe<Scalars['ID']['input']>;
+  id_gt?: InputMaybe<Scalars['ID']['input']>;
+  id_gte?: InputMaybe<Scalars['ID']['input']>;
+  id_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  id_lt?: InputMaybe<Scalars['ID']['input']>;
+  id_lte?: InputMaybe<Scalars['ID']['input']>;
+  id_not?: InputMaybe<Scalars['ID']['input']>;
+  id_not_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  nft?: InputMaybe<Scalars['String']['input']>;
+  nft_?: InputMaybe<Nft_Filter>;
+  nft_contains?: InputMaybe<Scalars['String']['input']>;
+  nft_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  nft_ends_with?: InputMaybe<Scalars['String']['input']>;
+  nft_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  nft_gt?: InputMaybe<Scalars['String']['input']>;
+  nft_gte?: InputMaybe<Scalars['String']['input']>;
+  nft_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  nft_lt?: InputMaybe<Scalars['String']['input']>;
+  nft_lte?: InputMaybe<Scalars['String']['input']>;
+  nft_not?: InputMaybe<Scalars['String']['input']>;
+  nft_not_contains?: InputMaybe<Scalars['String']['input']>;
+  nft_not_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  nft_not_ends_with?: InputMaybe<Scalars['String']['input']>;
+  nft_not_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  nft_not_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  nft_not_starts_with?: InputMaybe<Scalars['String']['input']>;
+  nft_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  nft_starts_with?: InputMaybe<Scalars['String']['input']>;
+  nft_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  or?: InputMaybe<Array<InputMaybe<Activity_Filter>>>;
+  price?: InputMaybe<Scalars['BigInt']['input']>;
+  price_gt?: InputMaybe<Scalars['BigInt']['input']>;
+  price_gte?: InputMaybe<Scalars['BigInt']['input']>;
+  price_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  price_lt?: InputMaybe<Scalars['BigInt']['input']>;
+  price_lte?: InputMaybe<Scalars['BigInt']['input']>;
+  price_not?: InputMaybe<Scalars['BigInt']['input']>;
+  price_not_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  timestamp?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_gt?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_gte?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  timestamp_lt?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_lte?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_not?: InputMaybe<Scalars['BigInt']['input']>;
+  timestamp_not_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  to?: InputMaybe<Scalars['Bytes']['input']>;
+  to_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  to_gt?: InputMaybe<Scalars['Bytes']['input']>;
+  to_gte?: InputMaybe<Scalars['Bytes']['input']>;
+  to_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  to_lt?: InputMaybe<Scalars['Bytes']['input']>;
+  to_lte?: InputMaybe<Scalars['Bytes']['input']>;
+  to_not?: InputMaybe<Scalars['Bytes']['input']>;
+  to_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  to_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  type?: InputMaybe<Scalars['String']['input']>;
+  type_contains?: InputMaybe<Scalars['String']['input']>;
+  type_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  type_ends_with?: InputMaybe<Scalars['String']['input']>;
+  type_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  type_gt?: InputMaybe<Scalars['String']['input']>;
+  type_gte?: InputMaybe<Scalars['String']['input']>;
+  type_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  type_lt?: InputMaybe<Scalars['String']['input']>;
+  type_lte?: InputMaybe<Scalars['String']['input']>;
+  type_not?: InputMaybe<Scalars['String']['input']>;
+  type_not_contains?: InputMaybe<Scalars['String']['input']>;
+  type_not_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  type_not_ends_with?: InputMaybe<Scalars['String']['input']>;
+  type_not_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  type_not_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  type_not_starts_with?: InputMaybe<Scalars['String']['input']>;
+  type_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  type_starts_with?: InputMaybe<Scalars['String']['input']>;
+  type_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+};
+
+export enum Activity_OrderBy {
+  From = 'from',
+  Id = 'id',
+  Nft = 'nft',
+  NftCollectionAddress = 'nft__collectionAddress',
+  NftCollectionName = 'nft__collectionName',
+  NftId = 'nft__id',
+  NftOwner = 'nft__owner',
+  NftPrice = 'nft__price',
+  NftUri = 'nft__uri',
+  Price = 'price',
+  Timestamp = 'timestamp',
+  To = 'to',
+  Type = 'type'
+}
+
+export type BlockChangedFilter = {
+  number_gte: Scalars['Int']['input'];
+};
+
+export type Block_Height = {
+  hash?: InputMaybe<Scalars['Bytes']['input']>;
+  number?: InputMaybe<Scalars['Int']['input']>;
+  number_gte?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Nft = {
+  __typename?: 'NFT';
+  activity: Array<Maybe<Activity>>;
+  collectionAddress: Scalars['Bytes']['output'];
+  collectionName: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  owner: Scalars['Bytes']['output'];
+  price?: Maybe<Scalars['BigInt']['output']>;
+  uri: Scalars['String']['output'];
+};
+
+
+export type NftActivityArgs = {
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Activity_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  where?: InputMaybe<Activity_Filter>;
+};
+
+export type Nft_Filter = {
+  /** Filter for the block changed event. */
+  _change_block?: InputMaybe<BlockChangedFilter>;
+  activity_?: InputMaybe<Activity_Filter>;
+  and?: InputMaybe<Array<InputMaybe<Nft_Filter>>>;
+  collectionAddress?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_gt?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_gte?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  collectionAddress_lt?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_lte?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_not?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  collectionAddress_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  collectionName?: InputMaybe<Scalars['String']['input']>;
+  collectionName_contains?: InputMaybe<Scalars['String']['input']>;
+  collectionName_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  collectionName_ends_with?: InputMaybe<Scalars['String']['input']>;
+  collectionName_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  collectionName_gt?: InputMaybe<Scalars['String']['input']>;
+  collectionName_gte?: InputMaybe<Scalars['String']['input']>;
+  collectionName_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  collectionName_lt?: InputMaybe<Scalars['String']['input']>;
+  collectionName_lte?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_contains?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_ends_with?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  collectionName_not_starts_with?: InputMaybe<Scalars['String']['input']>;
+  collectionName_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  collectionName_starts_with?: InputMaybe<Scalars['String']['input']>;
+  collectionName_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  id?: InputMaybe<Scalars['ID']['input']>;
+  id_gt?: InputMaybe<Scalars['ID']['input']>;
+  id_gte?: InputMaybe<Scalars['ID']['input']>;
+  id_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  id_lt?: InputMaybe<Scalars['ID']['input']>;
+  id_lte?: InputMaybe<Scalars['ID']['input']>;
+  id_not?: InputMaybe<Scalars['ID']['input']>;
+  id_not_in?: InputMaybe<Array<Scalars['ID']['input']>>;
+  or?: InputMaybe<Array<InputMaybe<Nft_Filter>>>;
+  owner?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_gt?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_gte?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  owner_lt?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_lte?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not_contains?: InputMaybe<Scalars['Bytes']['input']>;
+  owner_not_in?: InputMaybe<Array<Scalars['Bytes']['input']>>;
+  price?: InputMaybe<Scalars['BigInt']['input']>;
+  price_gt?: InputMaybe<Scalars['BigInt']['input']>;
+  price_gte?: InputMaybe<Scalars['BigInt']['input']>;
+  price_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  price_lt?: InputMaybe<Scalars['BigInt']['input']>;
+  price_lte?: InputMaybe<Scalars['BigInt']['input']>;
+  price_not?: InputMaybe<Scalars['BigInt']['input']>;
+  price_not_in?: InputMaybe<Array<Scalars['BigInt']['input']>>;
+  uri?: InputMaybe<Scalars['String']['input']>;
+  uri_contains?: InputMaybe<Scalars['String']['input']>;
+  uri_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  uri_ends_with?: InputMaybe<Scalars['String']['input']>;
+  uri_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  uri_gt?: InputMaybe<Scalars['String']['input']>;
+  uri_gte?: InputMaybe<Scalars['String']['input']>;
+  uri_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  uri_lt?: InputMaybe<Scalars['String']['input']>;
+  uri_lte?: InputMaybe<Scalars['String']['input']>;
+  uri_not?: InputMaybe<Scalars['String']['input']>;
+  uri_not_contains?: InputMaybe<Scalars['String']['input']>;
+  uri_not_contains_nocase?: InputMaybe<Scalars['String']['input']>;
+  uri_not_ends_with?: InputMaybe<Scalars['String']['input']>;
+  uri_not_ends_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  uri_not_in?: InputMaybe<Array<Scalars['String']['input']>>;
+  uri_not_starts_with?: InputMaybe<Scalars['String']['input']>;
+  uri_not_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+  uri_starts_with?: InputMaybe<Scalars['String']['input']>;
+  uri_starts_with_nocase?: InputMaybe<Scalars['String']['input']>;
+};
+
+export enum Nft_OrderBy {
+  Activity = 'activity',
+  CollectionAddress = 'collectionAddress',
+  CollectionName = 'collectionName',
+  Id = 'id',
+  Owner = 'owner',
+  Price = 'price',
+  Uri = 'uri'
+}
+
+/** Defines the order direction, either ascending or descending */
+export enum OrderDirection {
+  Asc = 'asc',
+  Desc = 'desc'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  /** Access to subgraph metadata */
+  _meta?: Maybe<_Meta_>;
+  activities: Array<Activity>;
+  activity?: Maybe<Activity>;
+  nft?: Maybe<Nft>;
+  nfts: Array<Nft>;
+};
+
+
+export type Query_MetaArgs = {
+  block?: InputMaybe<Block_Height>;
+};
+
+
+export type QueryActivitiesArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Activity_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<Activity_Filter>;
+};
+
+
+export type QueryActivityArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type QueryNftArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type QueryNftsArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Nft_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<Nft_Filter>;
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  /** Access to subgraph metadata */
+  _meta?: Maybe<_Meta_>;
+  activities: Array<Activity>;
+  activity?: Maybe<Activity>;
+  nft?: Maybe<Nft>;
+  nfts: Array<Nft>;
+};
+
+
+export type Subscription_MetaArgs = {
+  block?: InputMaybe<Block_Height>;
+};
+
+
+export type SubscriptionActivitiesArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Activity_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<Activity_Filter>;
+};
+
+
+export type SubscriptionActivityArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type SubscriptionNftArgs = {
+  block?: InputMaybe<Block_Height>;
+  id: Scalars['ID']['input'];
+  subgraphError?: _SubgraphErrorPolicy_;
+};
+
+
+export type SubscriptionNftsArgs = {
+  block?: InputMaybe<Block_Height>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+  orderBy?: InputMaybe<Nft_OrderBy>;
+  orderDirection?: InputMaybe<OrderDirection>;
+  skip?: InputMaybe<Scalars['Int']['input']>;
+  subgraphError?: _SubgraphErrorPolicy_;
+  where?: InputMaybe<Nft_Filter>;
+};
+
+export type _Block_ = {
+  __typename?: '_Block_';
+  /** The hash of the block */
+  hash?: Maybe<Scalars['Bytes']['output']>;
+  /** The block number */
+  number: Scalars['Int']['output'];
+  /** Integer representation of the timestamp stored in blocks for the chain */
+  timestamp?: Maybe<Scalars['Int']['output']>;
+};
+
+/** The type for the top-level _meta field */
+export type _Meta_ = {
+  __typename?: '_Meta_';
+  /**
+   * Information about a specific subgraph block. The hash of the block
+   * will be null if the _meta field has a block constraint that asks for
+   * a block number. It will be filled if the _meta field has no block constraint
+   * and therefore asks for the latest  block
+   *
+   */
+  block: _Block_;
+  /** The deployment ID */
+  deployment: Scalars['String']['output'];
+  /** If `true`, the subgraph encountered indexing errors at some past block */
+  hasIndexingErrors: Scalars['Boolean']['output'];
+};
+
+export enum _SubgraphErrorPolicy_ {
+  /** Data will be returned even if the subgraph has indexing errors */
+  Allow = 'allow',
+  /** If the subgraph has indexing errors, data will be omitted. The default. */
+  Deny = 'deny'
+}
+
+export type GetNftsQueryQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetNftsQueryQuery = { __typename?: 'Query', nfts: Array<{ __typename?: 'NFT', id: string, collectionAddress: any, owner: any, uri: string, collectionName: string, price?: any | null }>, activities: Array<{ __typename?: 'Activity', id: string, type: string, from: any, to: any, timestamp: any, price?: any | null }> };
+
+
+export const GetNftsQueryDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetNftsQuery"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"nfts"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"collectionAddress"}},{"kind":"Field","name":{"kind":"Name","value":"owner"}},{"kind":"Field","name":{"kind":"Name","value":"uri"}},{"kind":"Field","name":{"kind":"Name","value":"collectionName"}},{"kind":"Field","name":{"kind":"Name","value":"price"}}]}},{"kind":"Field","name":{"kind":"Name","value":"activities"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"type"}},{"kind":"Field","name":{"kind":"Name","value":"from"}},{"kind":"Field","name":{"kind":"Name","value":"to"}},{"kind":"Field","name":{"kind":"Name","value":"timestamp"}},{"kind":"Field","name":{"kind":"Name","value":"price"}}]}}]}}]} as unknown as DocumentNode<GetNftsQueryQuery, GetNftsQueryQueryVariables>;
