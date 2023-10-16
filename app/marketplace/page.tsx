de378ee6dc22194ee72b50fa7c01b28972237d06
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-10-16 14:23:37 +0700

    feat: added router to nft details page

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,28 +1,32 @@
 import Link from "next/link";
 
+const nft = {
+  tokenId: 1,
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-between p-24">
-      {/* <Link href={`./[assetId]/page.tsx`}> */}
-      <div className="flex flex-col border border-neutral-500 rounded-xl">
-        <img src="../nft.png" className="h-80 hover:transition" />
-        <div className="px-2 py-5 space-y-3">
-          <span className="">Bored Ape Yacht Club #7913</span>
-          <div className="flex justify-between">
-            <div className="flex items-center space-x-1 ">
-              <img
-                src="https://cryptologos.cc/logos/apecoin-ape-ape-logo.svg?v=026"
-                className="h-8"
-              />
-              <span className="text-lg">10,000</span>
+      <Link href={`./marketplace/${nft.tokenId}`}>
+        <div className="flex flex-col border border-neutral-500 rounded-xl">
+          <img src="../nft.png" className="h-80 hover:transition" />
+          <div className="px-2 py-5 space-y-3">
+            <span className="">Bored Ape Yacht Club #7913</span>
+            <div className="flex justify-between">
+              <div className="flex items-center space-x-1 ">
+                <img
+                  src="https://cryptologos.cc/logos/apecoin-ape-ape-logo.svg?v=026"
+                  className="h-8"
+                />
+                <span className="text-lg">10,000</span>
+              </div>
+              <button className="py-1 px-5 rounded-lg border border-neutral-600 hover:bg-neutral-600">
+                Buy
+              </button>
             </div>
-            <button className="py-1 px-5 rounded-lg border border-neutral-600 hover:bg-neutral-600">
-              Buy
-            </button>
           </div>
         </div>
-      </div>
-      {/* </Link> */}
+      </Link>
 
       <div className="flex flex-col border border-neutral-500 rounded-xl">
         <img src="../nft.png" className="h-80 hover:transition" />
