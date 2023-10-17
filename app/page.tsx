271fa965c6ab93db114d208ad0fc0dd103be494c
keivnjs <kjsurtan@gmail.com>
2023-10-18 01:30:57 +0700

    init Cometh Multichain Account contracts and connector

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import ConnectWallet from "@/components/ConnectWallet";
+import { Transaction } from "@/components/Transaction";
+import { useWalletAuth } from "@/lib/modules/wallet/hooks/useWalletAuth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const { isConnecting, isConnected, connect, connectionError, wallet } =
+    useWalletAuth();
+  const [transactionSuccess, setTransactionSuccess] = useState(false);
+
   return (
     <main className="flex min-h-screen items-center justify-center p-24">
-      <span>Register here</span>
+      <div>
+        <ConnectWallet
+          isConnected={isConnected}
+          isConnecting={isConnecting}
+          connect={connect}
+          connectionError={connectionError}
+          wallet={wallet!}
+        />
+
+        {isConnected && (
+          <Transaction
+            transactionSuccess={transactionSuccess}
+            setTransactionSuccess={setTransactionSuccess}
+          />
+        )}
+      </div>
     </main>
   );
 }
---
+++ lib/modules/wallet/getters/usePublicKey.ts
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+export interface Response {
+  publicKeyId: string;
+  publicKeyX: string;
+  publicKeyY: string;
+  deviceData: object;
+}
+
+export default async function usePublicKey(
+  apikey: string,
+  publicKeyId: string
+): Promise<any> {
+  const apiUrl = `https://api.connect.cometh.io/webauthn-signer/public-key-id/${publicKeyId}`;
+
+  const headers = {
+    apikey,
+  };
+
+  return axios
+    .get(apiUrl, { headers })
+    .then(function (res: any) {
+      let response: Response = {
+        publicKeyId: res.data.webAuthnSigner.publicKeyId,
+        publicKeyX: res.data.webAuthnSigner.publicKeyX,
+        publicKeyY: res.data.webAuthnSigner.publicKeyY,
+        deviceData: res.data.webAuthnSigner.deviceData,
+      };
+
+      return response;
+    })
+    .catch(function (error: string) {
+      console.error("API Error:", error);
+    });
+}
