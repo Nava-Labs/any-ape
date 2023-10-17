12ae4e7f4129005dcf8a45af89e3ffb9688f8169
Ryan Tjhin <ryan_tjhin@hotmail.com>
2023-10-17 17:02:28 +0700

    fi: deleted mock data and start using data from graph

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -12,36 +12,7 @@ export default async function Home() {
   const nftsRes = await client.query(getNftsQuery, {}).toPromise();
   if (!nftsRes) throw new Error("Failed to fetch NFTs");
 
-  // let nfts = nftsRes.data?.nfts;
-  let nfts = [
-    {
-      id: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd-123",
-      collectionAddress: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd",
-      owner: "0x37409a3e9542d4078d6aed82531e4eee9bba165a",
-      uri: "https://api.hv-mtl.com/hv/123",
-      collectionName: "HV-MTL",
-      price: "10,000",
-      __typename: "NFT",
-    },
-    {
-      id: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd-124",
-      collectionAddress: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd",
-      owner: "0x37409a3e9542d4078d6aed82531e4eee9bba165a",
-      uri: "https://api.hv-mtl.com/hv/124",
-      collectionName: "HV-MTL",
-      price: "20,000",
-      __typename: "NFT",
-    },
-    {
-      id: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd-125",
-      collectionAddress: "0x0cfb5d82be2b949e8fa73a656df91821e2ad99fd",
-      owner: "0x37409a3e9542d4078d6aed82531e4eee9bba165a",
-      uri: "https://api.hv-mtl.com/hv/125",
-      collectionName: "HV-MTL",
-      price: "30,000",
-      __typename: "NFT",
-    },
-  ];
+  let nfts = nftsRes.data?.listedNFTs;
 
   return (
     <>
@@ -59,7 +30,7 @@ export default async function Home() {
                     src="https://cryptologos.cc/logos/apecoin-ape-ape-logo.svg?v=026"
                     className="h-8"
                   />
-                  <span className="text-lg">{item.price}</span>
+                  <span className="text-lg">{Number(item.price) / 1e18}</span>
                 </div>
                 <button className="py-1 px-5 rounded-lg border border-neutral-600 hover:bg-neutral-600">
                   Buy
