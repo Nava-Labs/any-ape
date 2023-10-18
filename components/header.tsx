86f7abd5f5b4b447b632e6a049489ab9f1ef7ebc
keivnjs <kjsurtan@gmail.com>
2023-10-18 14:39:49 +0700

    feat: compose faucet claim tx & show account contract address

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { ComethWallet, SupportedNetworks } from "@cometh/connect-sdk";
+import {
+  SendTxArgs,
+  TxMetadata,
+  useSendTx,
+} from "@/lib/modules/wallet/hooks/useSendTx";
+import { encodeInputDataClaimFaucet } from "@/lib/utils";
 
 export function Header() {
+  const localStorageAddress = window.localStorage.getItem("walletAddress");
+
+  const mumbaiFaucet: TxMetadata = {
+    to: "0x8204C45D3Bc2Ecf24a4bc84c0c16426223b46877",
+    value: "0",
+    data: encodeInputDataClaimFaucet(),
+  };
+
+  const fujiFaucet: TxMetadata = {
+    to: "0xaB845a94e110D6f51815119ac3C87C6a268051Ee",
+    value: "0",
+    data: encodeInputDataClaimFaucet(),
+  };
+
+  const mumbaiFaucetTx: SendTxArgs = {
+    walletAddress: localStorageAddress!,
+    apiKey: process.env.NEXT_PUBLIC_COMETH_API_KEY_MUMBAI!,
+    chainId: SupportedNetworks.MUMBAI,
+    txMetadata: [mumbaiFaucet],
+  };
+
+  const fujiFaucetTx: SendTxArgs = {
+    walletAddress: localStorageAddress!,
+    apiKey: process.env.NEXT_PUBLIC_COMETH_API_KEY_FUJI!,
+    chainId: SupportedNetworks.FUJI,
+    txMetadata: [fujiFaucet],
+  };
+
+  const multiChainFaucet = () => {
+    useSendTx(mumbaiFaucetTx);
+    useSendTx(fujiFaucetTx);
+  };
+
   return (
     <div className="flex w-full items-center justify-between my-4">
       <div className="flex items-center">
@@ -15,7 +57,10 @@ export function Header() {
           <span className="text-base ml-4">Marketplace</span>
         </Link>
       </div>
-      <button>0xeD7B...6Afd</button>
+      <button>
+        {localStorageAddress!.slice(0, 5)}...{localStorageAddress?.slice(-4)}
+      </button>
+      <button onClick={() => multiChainFaucet()}>Faucet</button>
     </div>
   );
 }
