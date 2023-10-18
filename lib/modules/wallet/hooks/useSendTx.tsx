79b7514d87ea5df76911fd4e46731e1b32498a78
keivnjs <kjsurtan@gmail.com>
2023-10-18 22:21:59 +0700

    fix: return txhash & ccip txhash

--- lib/modules/wallet/hooks/useSendTx.tsx
+++ lib/modules/wallet/hooks/useSendTx.tsx
@@ -1,8 +1,10 @@
 import {
+  ComethProvider,
   ComethWallet,
   ConnectAdaptor,
   SupportedNetworks,
 } from "@cometh/connect-sdk";
+import { ethers } from "ethers";
 
 export type TxMetadata = {
   to: string;
@@ -16,6 +18,8 @@ export type SendTxArgs = {
   chainId: SupportedNetworks;
   txMetadata: TxMetadata[];
 };
+
+const anyApeFujiAddress = process.env.NEXT_PUBLIC_ANY_APE_FUJI!;
 
 export const useSendTx = async ({
   walletAddress,
@@ -33,17 +37,39 @@ export const useSendTx = async ({
     apiKey,
   });
 
-  console.log("conencted to fuji");
   await instance.connect(walletAddress);
-  console.log("tx done");
+
+  const provider = new ComethProvider(instance);
+
+  let txHash;
 
   if (txMetadata.length == 1) {
     const txValues = txMetadata[0];
-    console.log("txvalue", txValues);
-    await instance.sendTransaction(txValues);
+    const sendTx = await instance.sendTransaction(txValues);
+    const safeTxHash = sendTx.safeTxHash;
+
+    const txResponse = await provider.getTransaction(safeTxHash);
+    const txConfirmed = await txResponse.wait();
+
+    txHash = txConfirmed.transactionHash;
   } else {
     const txValues = txMetadata;
-    console.log("txvalue", txValues);
-    await instance.sendBatchTransactions(txValues);
+    const sendTx = await instance.sendBatchTransactions(txValues);
+    const safeTxHash = sendTx.safeTxHash;
+
+    const txResponse = await provider.getTransaction(safeTxHash);
+    const txConfirmed = await txResponse.wait();
+
+    txHash = txConfirmed.transactionHash;
+  }
+
+  if (txMetadata[0].to == anyApeFujiAddress) {
+    return "https://ccip.chain.link/tx/" + txHash;
+  }
+
+  if (chainId == SupportedNetworks.MUMBAI) {
+    return "https://mumbai.polygonscan.com/tx/" + txHash;
+  } else {
+    return "https://testnet.snowtrace.io/tx/" + txHash;
   }
 };
