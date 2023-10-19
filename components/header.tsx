702db491020c0f90eb62ece2365a1f074e358f09
keivnjs <kjsurtan@gmail.com>
2023-10-19 19:48:21 +0700

    feat: add tooltip at faucet

--- components/header.tsx
+++ components/header.tsx
@@ -6,9 +6,11 @@ import { approveAndClaim } from "@/lib/modules/wallet/hooks/approveAndClaim";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
+import * as Tooltip from "@radix-ui/react-tooltip";
 
 export function Header() {
   const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="flex w-full items-center justify-between my-4">
       <div className="flex items-center">
@@ -24,31 +26,43 @@ export function Header() {
         </Link>
       </div>
       <div className="flex space-x-3 ">
-        <button
-          className="bg-neutral-900 rounded-lg px-3 py-2 min-w-80"
-          disabled={isLoading}
-          onClick={async () => {
-            setIsLoading(true);
-            try {
-              await approveAndClaim();
-              toast.success(`Claim Successful.`, {
-                position: toast.POSITION.TOP_RIGHT,
-                closeOnClick: false,
-                hideProgressBar: true,
-              });
-            } catch (error) {
-              console.error(error);
-              toast.error("Claim Failed", {
-                position: toast.POSITION.TOP_RIGHT,
-                hideProgressBar: true,
-              });
-            } finally {
-              setIsLoading(false);
-            }
-          }}
-        >
-          {isLoading ? "Claiming multichain $BAPE..." : "Faucet"}
-        </button>
+        <Tooltip.Provider delayDuration={0}>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <button
+                className="bg-neutral-900 rounded-lg px-3 py-2 min-w-80"
+                disabled={isLoading}
+                onClick={async () => {
+                  setIsLoading(true);
+                  try {
+                    await approveAndClaim();
+                    toast.success(`Claim Successful.`, {
+                      position: toast.POSITION.TOP_RIGHT,
+                      closeOnClick: false,
+                      hideProgressBar: true,
+                    });
+                  } catch (error) {
+                    console.error(error);
+                    toast.error("Claim Failed", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      hideProgressBar: true,
+                    });
+                  } finally {
+                    setIsLoading(false);
+                  }
+                }}
+              >
+                {isLoading ? "Claiming multichain $BAPE..." : "Faucet"}
+              </button>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content className=" text-neutral-500 bg-black ">
+                Need 2 signature to claim.
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+
         <ToastContainer />
         <Account />
       </div>
